1921ca1fca6162a7e90eb0888af7952cb662498b
unknown <firmanajah366@gmail.com>
2026-05-25 22:34:19 +0700

    Menambah orbit loading spinner

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -6,6 +6,8 @@ export default function LoadingSpinner({ fullPage = false }) {
   return (
     <div className={`loading-spinner-container ${fullPage ? "full-page" : ""}`}>
       <div className="logo-loading-box">
+        <div className="orbit orbit-1"></div>
+        <div className="orbit orbit-2"></div>
         <img src={logoImg} alt="LeMaS Logo" className="logo-loading-img" />
       </div>
     </div>
